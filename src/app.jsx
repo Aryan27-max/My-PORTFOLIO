c59c9649b269df76cdd61935131a258d200e412d
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-08-30 14:07:00 +0530

    Initial 3d portfolio !!

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1>Welcome to my3d portfolio</h1>
+            </div>
+        </main>
+    )
+}
+export default App
