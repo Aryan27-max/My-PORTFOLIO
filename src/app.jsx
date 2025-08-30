42b976775ca9deb03136ea23c6170741919b4530
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-08-30 14:26:37 +0530

    index.css

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <main>
             <div>
-                <h1>Welcome to my3d portfolio</h1>
+                <h1>Welcome to my 3d portfolio !</h1>
             </div>
         </main>
     )
