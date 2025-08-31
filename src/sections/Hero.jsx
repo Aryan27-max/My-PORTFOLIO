ef3bc20d3aa2609bd88688992080188f9a498b57
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-08-31 17:18:42 +0530

    Button.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import {words} from '../constants/index.js'
 import React from 'react'
+import Button from '../components/Button.jsx'
 
 const Hero = () => {
     return (
@@ -32,6 +33,14 @@ const Hero = () => {
                             <h1>into Real Projects</h1>
                             <h1>that Deliver Results</h1>
                         </div>
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                            Hi, am Aryan a developer based in India with a passion for code.
+                        </p>
+                        <Button
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        id="button"
+                        text="See my Work"
+                        />
                     </div>
                 </header>
                 {/*RIGHT: 3D MODEL*/}
