8fca12b7fa0e7c341fed699656a3adb07f442773
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-09-01 00:37:40 +0530

    Updates and Particles.jsx

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -3,6 +3,7 @@ import {OrbitControls} from "@react-three/drei";
 import {useMediaQuery} from "react-responsive";
 import {Room} from "./Room.jsx";
 import HeroLights from "./HeroLights.jsx";
+import Particles from "./Particles.jsx";
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
@@ -21,6 +22,8 @@ const HeroExperience = () => {
             maxPolarAngle={Math.PI / 2}
             />
             <HeroLights />
+            <Particles count={500}/>
+
             <group
             scale={isMobile? 0.7 : 1}
             position={[0,-3.5,0]}
