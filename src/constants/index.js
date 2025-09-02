016e742cc54d861363dd1bb38d4531d26164e5a4
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-09-03 04:09:51 +0530

    SHowcaseSection.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ const words = [
 ];
 
 const counterItems = [
-    { value: 15, suffix: "+", label: "Years of Experience" },
-    { value: 200, suffix: "+", label: "Satisfied Clients" },
-    { value: 108, suffix: "+", label: "Completed Projects" },
-    { value: 90, suffix: "%", label: "Client Retention Rate" },
+    { value: 200, suffix: "+", label: "Github commits" },
+    { value: 12, suffix: "+", label: "Hackathons" },
+    { value: 20, suffix: "+", label: "Completed Projects" },
+    { value: 2, suffix: "+", label: "Years of Experience" },
 ];
 
 const logoIconsList = [
