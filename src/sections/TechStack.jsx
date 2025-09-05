19babd225d7eaf44d252ea35458ee61e0c6ab58f
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-09-05 23:51:21 +0530

    initial TechIcon.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import TitleHeader from "../components/TitleHeader.jsx";
+import {techStackIcons} from "../constants/index.js";
+import TechIcon from "../components/Models/TechLogos/TechIcon.jsx";
 
 const TechStack = () => {
     return (
-        <div>TechStack</div>
+        <div id="skills" className="flex-center section-padding">
+            <div className="w-full h-full md:px-10 px-5">
+                <TitleHeader
+                    title="My Prefered Tech Stack"
+                    sub=" Skills i bring to the table"
+                />
+
+                <div className="techgrid">
+                    {techStackIcons.map((techStackIcons) => (
+                        <div
+                            key={techStackIcons.name}
+                            className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
+                            <div className="tech-card-animated-bg" />
+                            <div className="tech-card-content">
+                                <div className="tech-icon-wrapper">
+                                    <TechIcon model={techStackIcons}/>
+                                </div>
+                                <div className="padding-x w-full">
+                                    <p>{techStackIcons.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default TechStack
